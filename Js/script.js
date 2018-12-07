a8f3ef8f588ed0c7869feb4f9998dcc75ccf8ead
javidLalayev <cavidleleyev9a12014@gmail.com>
2018-12-06 21:46:45 -0800

    ish

---
+++ Js/script.js
@@ -0,0 +1,17 @@
+function klikle(){
+	let root = document.documentElement;
+
+
+		root.style.setProperty('--color', 'black');
+		root.style.setProperty('--color2', 'white');
+
+}
+
+
+function klikle2(){
+	let root = document.documentElement;
+
+	root.style.setProperty('--color', 'white');
+	root.style.setProperty('--color2', 'black');
+
+}
